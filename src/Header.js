94efeba4cf7b8e4d40f87fe8779260a67b71f1d2
Hailemedhn <106559492+Hailemedhn@users.noreply.github.com>
2023-06-24 11:00:25 +0200

    cart done

--- src/Header.js
+++ src/Header.js
@@ -3,27 +3,31 @@ import "./Header.css";
 import manny from "./images/manny.png.png";
 import logo from "./images/logo.jpg";
 import cart from "./images/cart.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 class Header extends Component{
-
+    state = {
+        totalInCart:"0"
+    };
+    
     render(){
         return(
             <div className="containerh">
                 <div className="item1">
-                    <div className="humberger">
-                        <p className="p1"></p>
-                        <p className="p2"></p>
-                        <p className="p3"></p>
-                    </div>
+                    <FontAwesomeIcon icon={faBars}  size="3x" />
                 </div>
                 <div className="item2">
-                    <img className="manny" src={manny} alt="manny" />
+                    <FontAwesomeIcon icon={faUser}  size="3x"/>
                 </div>
                 <div className="item3">
                     <img className="logo" src={logo} alt="logo" />
                 </div>
                 <div className="item44">
-                    <img className="cart" src={cart} alt="cart" />
+                    <FontAwesomeIcon icon={faShoppingCart}  size="3x"/>
+                    <div className="total">{this.state.totalInCart}</div>
                 </div>
             </div>
         )
